384949eb0d21f5950ed7e047dd8360158ba807c1
Ilja <pman89@ya.ru>
2014-01-19 01:13:52 +0400

    carusel partial working

--- js/main.coffee
+++ js/main.coffee
@@ -1,5 +1,9 @@
 $ ->
 
+  #
+  # Pop up part
+  #
+
   popup           = $('.register-popup')
   popupContainer  = $('.popup-overlay')
   popupTrigger    = $('header > .top-info .enter-link')
@@ -32,3 +36,94 @@ $ ->
     elem = $ e.target
     if elem.hasClass 'popup-overlay'
       popupContainer.hide()
+
+  #
+  # Carusel part
+  #
+
+  caruselSlides             = $('.banner-container .full-banners > .single-banner')
+  caruselSlidesContainer    = $('.banner-container .full-banners')
+  caruselPreviews           = $('.banners-navigation .banners-preview > .single-banner-preview')
+  caruselPreviewsContainer  = $('.banners-navigation .banners-preview')
+  caruselNavigation         = $('.banner-container > .banners-navigation > .prev-banner, .banner-container > .banners-navigation > .next-banner')
+  currentSlideIndex         = 0
+  animInProgress            = false
+  visiblePreviewCount       = 5
+  animationSpeed            = 500
+
+  caruselNavigation.click (e) ->
+    e.preventDefault()
+
+    arrow = $ @
+    if arrow.hasClass 'next-banner'
+      if currentSlideIndex < caruselSlides.length - 1
+        animateToSlide currentSlideIndex + 1
+      else
+        animateToSlide 0
+    else
+      if currentSlideIndex > 0
+        animateToSlide currentSlideIndex - 1
+      else
+        animateToSlide caruselSlides.length - 1
+
+  caruselPreviewsContainer.on 'click', ".single-banner-preview", (e) ->
+    e.preventDefault()
+
+    preview = $ @
+    newIndex = preview.data('index')
+    animateToSlide newIndex
+
+
+  animateToSlide = (newIndex) ->
+    unless animInProgress
+
+      animInProgress = true
+
+      if newIndex isnt currentSlideIndex
+        caruselPreviews = $('.banners-navigation .banners-preview > .single-banner-preview')
+        currentSlide    = caruselSlides.filter("*[data-index=#{currentSlideIndex}]")
+        targetSlide     = caruselSlides.filter("*[data-index=#{newIndex}]")
+        currentPreview  = caruselPreviews.filter("*[data-index=#{currentSlideIndex}]")
+        targetPreview   = caruselPreviews.filter("*[data-index=#{newIndex}]")
+        tempTargetSlide = targetSlide.clone()
+        slideWidth      = targetSlide.width()
+        previewWidth    = targetPreview.width()
+        previewCount    = caruselPreviews.length
+
+        caruselPreviews.removeClass 'active'
+        targetPreview.addClass 'active'
+
+        animCallback = ->
+          currentSlideIndex = newIndex
+          tempTargetSlide.remove()
+          caruselSlidesContainer.css { left: "-#{ slideWidth * newIndex }px" }
+          animInProgress = false
+
+        completePreviews = ->
+          # FIXME: only one direction now, should be bi-directional
+          previewsLeftToDisplay = previewCount - targetPreview.index()
+          neededPreviews = if previewsLeftToDisplay <= visiblePreviewCount
+            visiblePreviewCount - previewsLeftToDisplay + 1
+          else
+            0
+          if neededPreviews > 0
+            newPreviews = caruselPreviews.slice(0, neededPreviews).clone()
+            caruselPreviewsContainer.append newPreviews
+          else
+            newPreviews = []
+
+          caruselPreviewsContainer.animate { left: "-#{ previewWidth * (neededPreviews)}px" }, animationSpeed, ->
+            if neededPreviews > 0
+              for i in [0..neededPreviews - 1]
+                $(caruselPreviews[i]).remove()
+              caruselPreviewsContainer.css { left: "0px" }
+
+        if newIndex > currentSlideIndex
+          tempTargetSlide.insertAfter currentSlide
+          caruselSlidesContainer.animate { left: "-=#{ slideWidth }" }, animationSpeed, animCallback
+          completePreviews()
+        else
+          tempTargetSlide.insertBefore currentSlide
+          caruselSlidesContainer.css { left: "-#{ slideWidth * (currentSlideIndex + 1) }px" }
+          caruselSlidesContainer.animate { left: "+=#{ slideWidth }" }, animationSpeed, animCallback
+          completePreviews()
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 (function() {
   $(function() {
-    var authContent, popup, popupContainer, popupTrigger, registerContent, tabLinks;
+    var animInProgress, animateToSlide, animationSpeed, authContent, caruselNavigation, caruselPreviews, caruselPreviewsContainer, caruselSlides, caruselSlidesContainer, currentSlideIndex, popup, popupContainer, popupTrigger, registerContent, tabLinks, visiblePreviewCount;
     popup = $('.register-popup');
     popupContainer = $('.popup-overlay');
     popupTrigger = $('header > .top-info .enter-link');
@@ -29,13 +29,114 @@
     popupTrigger.click(function(e) {
       return popupContainer.show();
     });
-    return popupContainer.click(function(e) {
+    popupContainer.click(function(e) {
       var elem;
       elem = $(e.target);
       if (elem.hasClass('popup-overlay')) {
         return popupContainer.hide();
       }
     });
+    caruselSlides = $('.banner-container .full-banners > .single-banner');
+    caruselSlidesContainer = $('.banner-container .full-banners');
+    caruselPreviews = $('.banners-navigation .banners-preview > .single-banner-preview');
+    caruselPreviewsContainer = $('.banners-navigation .banners-preview');
+    caruselNavigation = $('.banner-container > .banners-navigation > .prev-banner, .banner-container > .banners-navigation > .next-banner');
+    currentSlideIndex = 0;
+    animInProgress = false;
+    visiblePreviewCount = 5;
+    animationSpeed = 500;
+    caruselNavigation.click(function(e) {
+      var arrow;
+      e.preventDefault();
+      arrow = $(this);
+      if (arrow.hasClass('next-banner')) {
+        if (currentSlideIndex < caruselSlides.length - 1) {
+          return animateToSlide(currentSlideIndex + 1);
+        } else {
+          return animateToSlide(0);
+        }
+      } else {
+        if (currentSlideIndex > 0) {
+          return animateToSlide(currentSlideIndex - 1);
+        } else {
+          return animateToSlide(caruselSlides.length - 1);
+        }
+      }
+    });
+    caruselPreviewsContainer.on('click', ".single-banner-preview", function(e) {
+      var newIndex, preview;
+      e.preventDefault();
+      preview = $(this);
+      newIndex = preview.data('index');
+      return animateToSlide(newIndex);
+    });
+    return animateToSlide = function(newIndex) {
+      var animCallback, completePreviews, currentPreview, currentSlide, previewCount, previewWidth, slideWidth, targetPreview, targetSlide, tempTargetSlide;
+      if (!animInProgress) {
+        animInProgress = true;
+        if (newIndex !== currentSlideIndex) {
+          caruselPreviews = $('.banners-navigation .banners-preview > .single-banner-preview');
+          currentSlide = caruselSlides.filter("*[data-index=" + currentSlideIndex + "]");
+          targetSlide = caruselSlides.filter("*[data-index=" + newIndex + "]");
+          currentPreview = caruselPreviews.filter("*[data-index=" + currentSlideIndex + "]");
+          targetPreview = caruselPreviews.filter("*[data-index=" + newIndex + "]");
+          tempTargetSlide = targetSlide.clone();
+          slideWidth = targetSlide.width();
+          previewWidth = targetPreview.width();
+          previewCount = caruselPreviews.length;
+          caruselPreviews.removeClass('active');
+          targetPreview.addClass('active');
+          animCallback = function() {
+            currentSlideIndex = newIndex;
+            tempTargetSlide.remove();
+            caruselSlidesContainer.css({
+              left: "-" + (slideWidth * newIndex) + "px"
+            });
+            return animInProgress = false;
+          };
+          completePreviews = function() {
+            var neededPreviews, newPreviews, previewsLeftToDisplay;
+            previewsLeftToDisplay = previewCount - targetPreview.index();
+            neededPreviews = previewsLeftToDisplay <= visiblePreviewCount ? visiblePreviewCount - previewsLeftToDisplay + 1 : 0;
+            if (neededPreviews > 0) {
+              newPreviews = caruselPreviews.slice(0, neededPreviews).clone();
+              caruselPreviewsContainer.append(newPreviews);
+            } else {
+              newPreviews = [];
+            }
+            return caruselPreviewsContainer.animate({
+              left: "-" + (previewWidth * neededPreviews) + "px"
+            }, animationSpeed, function() {
+              var i, _i, _ref;
+              if (neededPreviews > 0) {
+                for (i = _i = 0, _ref = neededPreviews - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
+                  $(caruselPreviews[i]).remove();
+                }
+                return caruselPreviewsContainer.css({
+                  left: "0px"
+                });
+              }
+            });
+          };
+          if (newIndex > currentSlideIndex) {
+            tempTargetSlide.insertAfter(currentSlide);
+            caruselSlidesContainer.animate({
+              left: "-=" + slideWidth
+            }, animationSpeed, animCallback);
+            return completePreviews();
+          } else {
+            tempTargetSlide.insertBefore(currentSlide);
+            caruselSlidesContainer.css({
+              left: "-" + (slideWidth * (currentSlideIndex + 1)) + "px"
+            });
+            caruselSlidesContainer.animate({
+              left: "+=" + slideWidth
+            }, animationSpeed, animCallback);
+            return completePreviews();
+          }
+        }
+      }
+    };
   });
 
 }).call(this);
